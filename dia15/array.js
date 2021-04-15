96e3087b4bd1e57caacfeca7aec20a6abb6d2888
unknown <39702576+luanzinhoddk@users.noreply.github.com>
2021-04-14 22:08:58 -0300

    Array/Repetição

---
+++ dia15/array.js
@@ -0,0 +1,29 @@
+var jogos = ['CSGO', 'LOL', 'GTAV'];
+jogos.push("FIFA21");
+jogos.pop();
+
+for(i=0; i < jogos.length; i++){
+    console.log(jogos[i]);
+}
+
+var array = ['String0'];
+
+for(i=0; i< 50; i++){
+    array[i] = ("String" + i)
+}
+
+for(i=0; i<array.length; i++){
+    console.log(array[i]);
+}
+
+var a = "String24";
+
+var posicao = array.indexOf(a);
+console.log(array[posicao]);
+
+
+
+
+
+
+
